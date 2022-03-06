927c5d01996cf989818cd38eb9c2ae7c392ac278
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-06 22:00:53 +0530

    created counter reducer

--- src/counterContext.js
+++ src/counterContext.js
@@ -1,18 +1,11 @@
 import { createContext, useContext, useReducer } from "react";
+import { reducer } from "./counterReducer";
 
-const CounterContext = createContext();
+const CounterContext = createContext(null);
 
 const useCounter = () => useContext(CounterContext);
 
 const CounterProvider = ({ children }) => {
-  function reducer(state, action) {
-    switch (action.type) {
-      case "increment":
-        return { count: state.count + action.payload };
-      case "decrement":
-        return { count: state.count - action.payload };
-    }
-  }
   const [state, dispatcher] = useReducer(reducer, { count: 0 });
   return (
     <CounterContext.Provider value={{ state, dispatcher }}>
---
+++ src/counterReducer.jsx
@@ -0,0 +1,8 @@
+export function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + action.payload };
+    case "decrement":
+      return { count: state.count - action.payload };
+  }
+}
