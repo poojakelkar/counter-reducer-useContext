84e103c9219948e9b5c057afb85343ed539da0f8
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-07 18:14:51 +0530

    creating add-to-cart file

--- src/counterReducer.jsx
+++
@@ -1,8 +0,0 @@
-export function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + action.payload };
-    case "decrement":
-      return { count: state.count - action.payload };
-  }
-}
